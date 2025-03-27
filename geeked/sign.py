9f8ddfd8928cfce35b30d8bc054bd51bd7a7b794
xKiian <xKiian@users.noreply.github.com>
2025-03-27 12:23:43 +0000

    Update Geetest version to v1.8.8-1d0f51

--- geeked/sign.py
+++ geeked/sign.py
@@ -16,7 +16,7 @@ from geeked.gobang import GobangSolver
 
 class LotParser:
     def __init__(self):
-        self.mapping = {"(n[5:12])+.+(n[18:21]+n[20:23])":'n[16:23]'}
+        self.mapping = {"(n[6:13])+.+(n[15]+n[24]+n[25]+n[16])":'n[9:12]'}
         self.lot = []
         self.lot_res = []
         for k, v in self.mapping.items():
@@ -181,7 +181,7 @@ function encrypt_asymmetric_2(input, key) {
     def generate_w(data: dict, captcha_id: str, risk_type: str):
         lot_number = data['lot_number']
         pow_detail = data['pow_detail']
-        abo = {"aO98":"LmGR"}
+        abo = {"ipq9":"7hFw"}
         base = abo | {
             **Signer.generate_pow(lot_number, captcha_id, pow_detail['hashfunc'], pow_detail['version'],
                                   pow_detail['bits'], pow_detail['datetime'], ""),
